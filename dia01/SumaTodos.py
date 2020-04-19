a7a39dc7b90ff0af1a02f11de9cea1cdd8c918bb
pardo2410 <pardo2410@gmail.com>
2020-04-19 19:30:29 +0200

    Ajuste archivo SumaTodo

--- dia01/SumaTodos.py
+++ dia01/SumaTodos.py
@@ -1,8 +1,9 @@
 def sumaTodos(limit):
-    i=0
+    
     resultado=0
     for i in range(0,limit + 1):
-        resultado += 1
+        resultado += i
     return resultado
 
-print(sumaTodos(100))
+Num=int(input('ingrese un numero '))
+print(sumaTodos(Num))
