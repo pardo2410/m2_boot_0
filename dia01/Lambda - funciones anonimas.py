132847e09a0fb355d407af3589c4156f16aaaa92
pardo2410 <pardo2410@gmail.com>
2020-04-20 14:36:42 +0200

    Pruebas con lambdas

--- dia01/Lambda - funciones anonimas.py
+++ dia01/Lambda - funciones anonimas.py
@@ -3,3 +3,5 @@ from SumaTodoLambda import sumaTodos2
 
 valor=int(input('Ingrese el valor: '))
 print('Suma todos los cubos hasta el numero ',valor,' el resultado es: ' ,sumaTodos2(valor,lambda x: x**3))
+print('Suma todos los valores por 3 hasta el numero ',valor,' el resultado es: ' ,sumaTodos2(valor,lambda x: x*3))
+print('Suma todos los valores por 2 hasta el numero ',valor,' el resultado es: ' ,sumaTodos2(valor,lambda x: x*2))
