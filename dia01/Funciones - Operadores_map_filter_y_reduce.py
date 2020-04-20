ea5859c29d84b324a4f753d03632098252fe712e
pardo2410 <pardo2410@gmail.com>
2020-04-20 17:26:26 +0200

    Funciones - Operadores_map_filter_y_reduce.py

---
+++ dia01/Funciones - Operadores_map_filter_y_reduce.py
@@ -0,0 +1,28 @@
+lista = [1,3,-1,15,9,4]
+
+#map permite que una funcion actue sobre cada uno de los valores simplificando el proceso
+listaDobles = map(lambda x: x*2, lista)
+print(list(listaDobles))
+
+#filter se usa para filtrar segun la condicion definida, en este caso solo se imprimira los valores pares 
+
+#utilizando una funcion anonima lambda
+listaPares = filter(lambda x: x % 2 == 0,lista)
+print(list(listaPares))
+
+#escrita utilizando una funcion general
+def esImpar(x):
+    return x % 2 != 0
+
+listaImpares=filter(esImpar,lista)
+print(list(listaImpares))
+
+#reduce suma todos los valores de una lista o vector y da como resultado un unico valor
+from functools import reduce
+
+sumatoria = reduce(lambda x, y: x + y , lista)
+print(sumatoria)
+print('media ',sumatoria/len(lista))
+
+suma100 = reduce(lambda x,y: x + y,range(101))
+print(suma100)
